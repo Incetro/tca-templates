fbfd3ef4d88afd49ed023c1db5fff046faea0ba0
TraxxasWyls <savinov.dsta@gmail.com>
2023-10-19 14:56:10 +0300

    Added deafult module template

---
+++ DefatulModule/TemplateModuleAction.swift
@@ -0,0 +1,22 @@
+//
+//  TemplateModuleAction.swift
+//  TCATemplate
+//
+//  Created by Dmitry Savinov on 19.10.2023.
+//  Copyright © 2023 Incetro Inc. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - TemplateModuleAction
+
+public enum TemplateModuleAction: Equatable {
+    
+    // MARK: - Cases
+    
+    /// General action that calls when view appears
+    case onAppear
+    
+    /// General action that calls when view disappears
+    case onDisappear
+}
---
+++ DefatulModule/TemplateModuleState.swift
@@ -0,0 +1,16 @@
+//
+//  TemplateModuleState.swift
+//  TCATemplate
+//
+//  Created by Dmitry Savinov on 19.10.2023.
+//  Copyright © 2023 Incetro Inc. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - TemplateModuleState
+
+public struct TemplateModuleState: Equatable {
+    
+    // MARK: - Properties
+}
