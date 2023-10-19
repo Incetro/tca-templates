068d294ec0fca915ce72ab8d98e61406bba17ec1
TraxxasWyls <savinov.dsta@gmail.com>
2023-10-19 15:57:10 +0300

    Add id reloadable module

---
+++ IDRealoadableModule/TemplateModuleState.swift
@@ -0,0 +1,31 @@
+//
+//  TemplateModuleState.swift
+//  TCATemplate
+//
+//  Created by Dmitry Savinov on 19.10.2023.
+//  Copyright © 2023 Incetro Inc. All rights reserved.
+//
+
+import Foundation
+import TCANetworkReducers
+
+// MARK: - TemplateModuleState
+
+public struct TemplateModuleState: Equatable {
+    
+    // MARK: - Properties
+    
+    // MARK: - Relodable
+    
+    /// ReloadableState instace for network operations
+    public var reloadableTemplateModule: IDReloadableState<String, String, NSError>
+}
+
+// MARK: - Initializers
+
+extension TemplateModuleState {
+ 
+    public init() {
+        self.reloadableTemplateModule = IDReloadableState(id: "")
+    }
+}
